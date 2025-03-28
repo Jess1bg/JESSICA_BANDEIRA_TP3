c0966647c90036c793d7ce8d467194cbb55c567d
Jéssica Bandeira Gomes <jessica.gomes@al.infnet.edu.br>
2025-03-28 18:28:44 -0300

    TP3

---
+++ Exercicio07/Matricula.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Matricula
+{
+    public string NomeDoAluno { get; set; }
+    public string Curso { get; set; }
+    public int NumeroMatricula { get; set; }
+    public string Situacao { get; set; }
+    public string DataInicial { get; set; }
+
+    public Matricula()
+    {
+        Situacao = "Ativa";
+    }
+
+    public Matricula(string nome, string curso, int numeroMatricula, string dataInicial)
+    {
+        NomeDoAluno = nome;
+        Curso = curso;
+        NumeroMatricula = numeroMatricula;
+        DataInicial = dataInicial;
+        Situacao = "Ativa"; 
+    }
+}
+
+
+
+
---
+++ Exercicio07/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Matricula aluno1 = new Matricula(
+            "Jéssica Bandeira",
+            "Analise e Desenvolvimento de Sistemas",
+            20240001,
+            "29/02/2023"
+        );
+
+        aluno1.Situacao = "Ativa";
+
+        Console.WriteLine("--- DADOS DA MATRÍCULA ---");
+        Console.WriteLine($"Aluno: {aluno1.NomeDoAluno}");
+        Console.WriteLine($"Curso: {aluno1.Curso}");
+        Console.WriteLine($"Nº Matrícula: {aluno1.NumeroMatricula}");
+        Console.WriteLine($"Situação: {aluno1.Situacao}");
+        Console.WriteLine($"Data Inicial: {aluno1.DataInicial}");
+
+        Console.WriteLine("\nPressione ENTER para sair...");
+        Console.ReadLine();
+    }
+}
