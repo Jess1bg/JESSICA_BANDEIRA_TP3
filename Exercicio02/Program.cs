eacaf6c193bbfd3eda11001e0d67f972e87eb949
Jéssica Bandeira Gomes <jessica.gomes@al.infnet.edu.br>
2025-03-28 13:26:12 -0300

    TP3

---
+++ Exercicio02/Program.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+class Ingresso
+{
+    public string nomeDoShow; // Esse atributo identifica qual evento ou show pertence o ingresso que esta sendo vendido. 
+    public double preco; // Utilizado para definir o valor do ingresso, é importante para o cálculo de vendas e lucros.
+    public int quantidadeDisponivel; // Exibe a qunatidade de ingressos disponiveis, com isso é possivel controlar o estoque para evitar vender mais ingressos do que o disponível.
+}
