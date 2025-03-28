3e4b77ffc6b53c0a23bc4f78c6331ba99d500d29
Jéssica Bandeira Gomes <jessica.gomes@al.infnet.edu.br>
2025-03-28 13:21:05 -0300

    TP3

---
+++ Exercicio01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class Carro
+{
+    public string Cor;
+    public int Potencia;
+
+    public void Apresentar()
+    {
+        Console.WriteLine($"Este carro tem cor {Cor} e {Potencia}CV de potência.");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Carro hilux = new Carro();
+
+        hilux.Cor = "Prata";
+        hilux.Potencia = 204;
+
+        hilux.Apresentar();
+
+    }
+}
