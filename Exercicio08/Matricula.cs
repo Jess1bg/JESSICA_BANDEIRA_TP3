b8cadf5ee87a8f8dba70a4800ac2e910bbfd688e
Jéssica Bandeira Gomes <jessica.gomes@al.infnet.edu.br>
2025-03-28 18:39:44 -0300

    TP3

---
+++ Exercicio08/Matricula.cs
@@ -0,0 +1,36 @@
+﻿class Matricula
+{
+    public string NomeDoAluno { get; set; }
+    public string Curso { get; set; }
+    public int NumeroMatricula { get; set; }
+    public string Situacao { get; set; }
+    public string DataInicial { get; set; }
+
+    public Matricula(string nome, string curso, int numeroMatricula, string dataInicial)
+    {
+        NomeDoAluno = nome;
+        Curso = curso;
+        NumeroMatricula = numeroMatricula;
+        DataInicial = dataInicial;
+        Situacao = "Ativa"; 
+    }
+
+    public void Trancar()
+    {
+        Situacao = "Trancada";
+    }
+
+    public void Reativar()
+    {
+        Situacao = "Ativa";
+    }
+
+    public void ExibirInformacoes()
+    {
+        Console.WriteLine($"Aluno: {NomeDoAluno}");
+        Console.WriteLine($"Curso: {Curso}");
+        Console.WriteLine($"Nº Matrícula: {NumeroMatricula}");
+        Console.WriteLine($"Situação: {Situacao}");
+        Console.WriteLine($"Data Inicial: {DataInicial}");
+    }
+}
---
+++ Exercicio08/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Matricula aluno1 = new Matricula(
+            "Patricia Junqueira",
+            "Engenharia de Petroleo",
+            2036007,
+            "20/03/2022"
+        );
+
+        Console.WriteLine("--- MATRÍCULA CRIADA ---");
+        aluno1.ExibirInformacoes();
+
+        aluno1.Trancar();
+        Console.WriteLine("\n--- APÓS TRANCAR ---");
+        aluno1.ExibirInformacoes();
+
+        aluno1.Reativar();
+        Console.WriteLine("\n--- APÓS REATIVAR ---");
+        aluno1.ExibirInformacoes();
+
+        Console.WriteLine("\nPressione ENTER para sair...");
+        Console.ReadLine();
+    }
+}
