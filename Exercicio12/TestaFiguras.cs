7edc13a179c63e64e2394f0670438bb5ee1b47db
Jéssica Bandeira Gomes <jessica.gomes@al.infnet.edu.br>
2025-03-28 20:55:59 -0300

    TP3

---
+++ Exercicio12/TestaFiguras.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class TestaFiguras
+{
+    static void Main()
+    {
+        Circulo circulo = new Circulo();
+        circulo.Raio = 3.0;
+        double area = circulo.CalcularArea();
+        Console.WriteLine("=== TESTE DO CÍRCULO ===");
+        Console.WriteLine($"Raio: {circulo.Raio}");
+        Console.WriteLine($"Área: {area:F2}\n");
+
+        Esfera esfera = new Esfera();
+        esfera.Raio = 5.0;
+        double volume = esfera.CalcularVolume();
+        Console.WriteLine("=== TESTE DA ESFERA ===");
+        Console.WriteLine($"Raio: {esfera.Raio}");
+        Console.WriteLine($"Volume: {volume:F2}");
+
+        Console.WriteLine("\nPressione ENTER para sair...");
+        Console.ReadLine();
+    }
+}
