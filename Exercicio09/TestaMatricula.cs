859c060799bf459468b3a7cd5238bb734e9ec58f
Jéssica Bandeira Gomes <jessica.gomes@al.infnet.edu.br>
2025-03-28 20:23:47 -0300

    TP3

---
+++ Exercicio09/TestaMatricula.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class TestaMatricula
+{
+    static void Main()
+    {
+        Matricula aluno = new Matricula(
+            "Fernando Garcia",
+            "Arquiteto",
+            20490040,
+            "03/05/2024"
+        );
+
+        Console.WriteLine("=== SITUAÇÃO INICIAL ===");
+        aluno.ExibirInformacoes();
+
+        aluno.Trancar();
+        Console.WriteLine("=== APÓS TRANSCAR ===");
+        aluno.ExibirInformacoes();
+
+        aluno.Reativar();
+        Console.WriteLine("=== APÓS REATIVAR ===");
+        aluno.ExibirInformacoes();
+
+        Console.WriteLine("Pressione ENTER para encerrar...");
+        Console.ReadLine();
+    }
+}
