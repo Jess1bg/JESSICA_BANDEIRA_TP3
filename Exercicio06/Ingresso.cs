288c33ae8050f2f149c120647907bd26a8598337
Jéssica Bandeira Gomes <jessica.gomes@al.infnet.edu.br>
2025-03-28 18:08:27 -0300

    TP3

---
+++ Exercicio06/Ingresso.cs
@@ -0,0 +1,31 @@
+﻿class Ingresso
+{
+    public string nomeDoShow;
+    public double preco;
+    public int quantidadeDisponivel;
+
+    public Ingresso(string nome, double valor, int quantidade)
+    {
+        nomeDoShow = nome;
+        preco = valor;
+        quantidadeDisponivel = quantidade;
+    }
+
+    public string GetNomeDoShow() => nomeDoShow;
+    public double GetPreco() => preco;
+    public int GetQuantidadeDisponivel() => quantidadeDisponivel;
+
+    public void SetNomeDoShow(string novoNome) => nomeDoShow = novoNome;
+    public void SetPreco(double novoPreco) => preco = novoPreco;
+    public void SetQuantidadeDisponivel(int novaQtd) => quantidadeDisponivel = novaQtd;
+
+    public void AlterarPreco(double novoPreco) => preco = novoPreco;
+    public void AlterarQuantidade(int novaQuantidade) => quantidadeDisponivel = novaQuantidade;
+
+    public void ExibirInformacoes()
+    {
+        Console.WriteLine($"Nome do Show: {nomeDoShow}");
+        Console.WriteLine($"Preço: R$ {preco:F2}");
+        Console.WriteLine($"Quantidade Disponível: {quantidadeDisponivel}");
+    }
+}
---
+++ Exercicio06/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Ingresso show = new Ingresso(
+            "Show do Avenged Sevenfold",
+            650.00,
+            8000
+        );
+
+        Console.WriteLine("--- TESTANDO GETTERS ---");
+        Console.WriteLine($"Nome: {show.GetNomeDoShow()}");
+        Console.WriteLine($"Preço: {show.GetPreco():C2}");
+        Console.WriteLine($"Quantidade: {show.GetQuantidadeDisponivel()}");
+
+        show.SetPreco(700.00);
+        show.SetQuantidadeDisponivel(7500);
+
+        show.AlterarPreco(750.00);
+        show.AlterarQuantidade(7000);
+
+        Console.WriteLine("\n--- DADOS FINAIS ---");
+        show.ExibirInformacoes();
+
+        Console.WriteLine("\nPressione ENTER para sair...");
+        Console.ReadLine();
+    }
+}
