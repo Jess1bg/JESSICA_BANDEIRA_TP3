4e8ef2afa4ffa00be0dc044456cd78944edb8216
Jéssica Bandeira Gomes <jessica.gomes@al.infnet.edu.br>
2025-03-28 17:54:54 -0300

    TP3

---
+++ Exercicio05/Ingresso.cs
@@ -0,0 +1,43 @@
+﻿class Ingresso
+{
+    public string nomeDoShow;
+    public double preco;
+    public int quantidadeDisponivel;
+
+    public string GetNomeDoShow()
+    {
+        return nomeDoShow;
+    }
+
+    public double GetPreco()
+    {
+        return preco;
+    }
+
+    public int GetQuantidadeDisponivel()
+    {
+        return quantidadeDisponivel;
+    }
+
+    public void SetNomeDoShow(string novoNome)
+    {
+        nomeDoShow = novoNome;
+    }
+
+    public void SetPreco(double novoPreco)
+    {
+        preco = novoPreco;
+    }
+
+    public void SetQuantidadeDisponivel(int novaQtd)
+    {
+        quantidadeDisponivel = novaQtd;
+    }
+
+    public void ExibirInformacoes()
+    {
+        Console.WriteLine($"Nome do Show: {nomeDoShow}");
+        Console.WriteLine($"Preço: R$ {preco:F2}");
+        Console.WriteLine($"Quantidade Disponível: {quantidadeDisponivel}");
+    }
+}
---
+++ Exercicio05/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Ingresso show = new Ingresso();
+
+        show.SetNomeDoShow("Show do Avenged Sevenfold");
+        show.SetPreco(490.00);
+        show.SetQuantidadeDisponivel(15000);
+
+        Console.WriteLine("--- DADOS INICIAIS (VIA GETTERS) ---");
+        Console.WriteLine($"Nome: {show.GetNomeDoShow()}");
+        Console.WriteLine($"Preço: R$ {show.GetPreco():F2}");
+        Console.WriteLine($"Quantidade: {show.GetQuantidadeDisponivel()}");
+
+        show.SetPreco(650.00);
+        show.SetQuantidadeDisponivel(10000);
+
+        Console.WriteLine("\n--- DADOS ATUALIZADOS (VIA MÉTODO) ---");
+        show.ExibirInformacoes();
+
+        Console.WriteLine("\nPressione ENTER para sair...");
+        Console.ReadLine();
+    }
+}
